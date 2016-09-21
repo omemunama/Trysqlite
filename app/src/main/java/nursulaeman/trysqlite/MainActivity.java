5b4b290a43a3bfccf0867045331e2b57bb76ddb6
omemunama <xnursx@gmail.com>
2016-09-21 09:15:10 +0700

    TrySqlite ok

--- app/src/main/java/nursulaeman/trysqlite/MainActivity.java
+++ app/src/main/java/nursulaeman/trysqlite/MainActivity.java
@@ -21,8 +21,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        myDB = new DatabaseHelper(this);
         name = (EditText)findViewById(R.id.name_txt);
         surname = (EditText)findViewById(R.id.surname_txt);
         marks = (EditText)findViewById(R.id.marks_txt);
@@ -31,71 +29,79 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         list_btn = (Button)findViewById(R.id.students_btn);
         update_btn = (Button)findViewById(R.id.update_btn);
         delete_btn = (Button)findViewById(R.id.delete_btn);
+
+        //Listener
+        myDB = new DatabaseHelper(this);
         save_btn.setOnClickListener(this);
         list_btn.setOnClickListener(this);
         update_btn.setOnClickListener(this);
         delete_btn.setOnClickListener(this);
+        myDB.list_student();
 
     }
 
+
+
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.send_btn :
+        switch (v.getId()){
+            //SAVED
+            case R.id.send_btn:
                 boolean result = myDB.save_student(name.getText().toString(),
-                        surname.getText().toString(),
-                        marks.getText().toString()
-                );
-                if (result)
-                    Toast.makeText(MainActivity.this, "Success Add Student", Toast.LENGTH_LONG).show();
-                else
-                    Toast.makeText(MainActivity.this, "Fails Add Student", Toast.LENGTH_LONG).show();
-                break;
-            case R.id.students_btn :
-                Cursor students = myDB.list_student();
-                if (students.getCount() == 0) {
-                    alert_message("Message", "No data student found");
-                    return;
-                }
-
-                //append data student to buffer
-                StringBuffer buffer = new StringBuffer();
-                while (students.moveToNext()) {
-                    buffer.append("Id : " + students.getString(0) + "\n");
-                    buffer.append("Name : " + students.getString(1) + "\n");
-                    buffer.append("Surname : " + students.getString(2) + "\n");
-                    buffer.append("Marks : " + students.getString(3) + "\n\n\n");
-                }
-
-                //show data student
-                alert_message("List Students", buffer.toString());
-                break;
-            case R.id.update_btn :
-                boolean result = myDB.update_student(id.getText().toString(),
-                        name.getText().toString(),
                         surname.getText().toString(),
                         marks.getText().toString());
                 if (result)
-                    Toast.makeText(MainActivity.this, "Success update data Student", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "Succed Add Student", Toast.LENGTH_SHORT).show();
                 else
-                    Toast.makeText(MainActivity.this, "Fails update data Student", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "Failed Add Student", Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.delete_btn :
-                Integer result = myDB.delete_student(id.getText().toString());
-                if (result > 0)
-                    Toast.makeText(MainActivity.this, "Success delete a Student", Toast.LENGTH_LONG).show();
+            case R.id.students_btn:
+                Cursor students = myDB.list_student();
+                if (students.getCount()==0){
+                    alert_message("Message", "NO DATA STUDENTS FOUND");
+                    return;
+                }
+
+                //APPEND data student to buffer
+                StringBuffer buffer = new StringBuffer();
+                while (students.moveToNext()){
+                    buffer.append("Id : "+ students.getString(0) +"\n");
+                    buffer.append("Name : "+ students.getString(1) + "\n");
+                    buffer.append("Surname : "+ students.getString(2) +"\n");
+                    buffer.append("Marks : "+ students.getString(3) +"\n\n\n");
+                }
+
+                //SHOW DATA STUDENT
+                alert_message("List Students", buffer.toString());
+                break;
+
+            //UPDATE
+            case R.id.update_btn:
+                boolean update_result = myDB.update_student(id.getText().toString(),
+                        name.getText().toString(),
+                        surname.getText().toString(),
+                        marks.getText().toString());
+                if(update_result)
+                    Toast.makeText(MainActivity.this, "Succeed Update", Toast.LENGTH_SHORT).show();
+                break;
+
+            //DELETE
+            case R.id.delete_btn:
+                Integer delete_result = myDB.delete_student(id.getText().toString());
+                if(delete_result>0)
+                    Toast.makeText(MainActivity.this, "Succeed Delete", Toast.LENGTH_SHORT).show();
                 else
-                    Toast.makeText(MainActivity.this, "Fails delete a Student", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "Failed Delete", Toast.LENGTH_SHORT).show();
                 break;
         }
     }
 
-    public void alert_message(String title, String message) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setCancelable(true);
-        builder.setTitle(title);
-        builder.setMessage(message);
-        builder.show();
+    //SHOW ALERT
+    private void alert_message(String title, String message) {
+        AlertDialog.Builder build = new AlertDialog.Builder(this);
+        build.setCancelable(true);
+        build.setTitle(title);
+        build.setMessage(message);
+        build.show();
     }
-
 }
